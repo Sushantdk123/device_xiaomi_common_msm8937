3ff6af0accaaabe153333bc50a044cb1f24c5a0a
intervigilium <intervigil@gmail.com>
2020-01-31 14:36:26 +0000

    msm8937-common: Drop deprecated property
    * Don't set triple buffering, it introduces latency

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -192,5 +192,4 @@ persist.vendor.radio.aosp_usr_pref_sel=true
 
 # Configstore
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-ro.surface_flinger.max_virtual_display_dimension=4096 \
-ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+ro.surface_flinger.max_virtual_display_dimension=4096 
